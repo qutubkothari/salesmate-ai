0f4c7f50e801a180e3cb3cd34702e724798a7948
qutubkothari <kothariqutub@gmail.com>
2026-01-25 11:58:58 +0530

    Deploy Salesmate

--- deploy-salesmate-hostinger.ps1
+++ deploy-salesmate-hostinger.ps1
@@ -145,7 +145,7 @@ server {
 
     add_header X-Frame-Options "SAMEORIGIN" always;
     add_header X-Content-Type-Options "nosniff" always;
-    add_header X-XSS-Protection "1; mode=block" always;
+    add_header X-XSS-Protection "1" always;
     
     client_max_body_size 50M;
 }
